cca3ac416c16f1d9be28b65dd87692a1160e1f1e
C0L2 <vacara7samue7@gmail.com>
2023-11-25 01:34:27 +0200

    more on CORS

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,27 @@
 import * as cors from 'cors';
+import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { UserService } from './user/user.service';
 import { User } from './entities/user.entity';
 import { SurveyService } from './survey/survey.service';
-import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: 'https://coffik.netlify.app',
+  const allowedOrigins = ['https://coffik.netlify.app'];
+  app.use(cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  });
+  }));
 
   await app.listen(process.env.PORT, '0.0.0.0');
   const userService = app.get(UserService);
