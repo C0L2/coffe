0a8e6ce0719386e6a83c0f4e7e119da89bb31905
C0L2 <vacara7samue7@gmail.com>
2023-11-24 18:46:43 +0200

    deployment

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({
     origin: 'http://localhost:5173/',
+    credentials: true,
   });
   await app.listen(9500);
   const userService = app.get(UserService);
