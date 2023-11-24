d4d9aea74cd57d4d770ddaf756e209bbc6a634d1
C0L2 <vacara7samue7@gmail.com>
2023-11-24 19:33:08 +0200

    true added

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,7 +22,7 @@ import { ConfigModule } from '@nestjs/config';
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
       entities: [User, Questions, Answer, Question],
-      synchronize: false,
+      synchronize: true,
       ssl: true, //add in deployment
     }),
     UserModule,
