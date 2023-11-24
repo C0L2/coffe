56169b7a360092bd3587abdc088d49994cc8e5a5
C0L2 <vacara7samue7@gmail.com>
2023-11-24 19:20:28 +0200

    ui deploy

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://coffik.netlify.app/',
     credentials: true
   }));
   await app.listen(process.env.PORT || 9500);
