e658513458b5d7f9ffc9eb0a4348d72214272904
C0L2 <vacara7samue7@gmail.com>
2023-11-24 19:55:01 +0200

    .

--- src/main.ts
+++ src/main.ts
@@ -14,13 +14,7 @@ async function bootstrap() {
     credentials: true
   }));
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
-  await app.listen(process.env.PORT || 9500);
+  await app.listen(process.env.PORT, '0.0.0.0');
   const userService = app.get(UserService);
   const surveyService = app.get(SurveyService)
 
